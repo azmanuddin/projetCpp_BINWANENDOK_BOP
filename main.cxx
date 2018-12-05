aa59c6923d8efd567d9440fdb5721c8a89c66945
Azmanuddin <azman.ir93@gmail.com>
2018-12-05 12:05:53 +0100

    Create main.cxx

---
+++ main.cxx
@@ -0,0 +1,14 @@
+/**
+    main.cxx
+    But: Implémenter l'algorithme de Dijkstra en C ++ pour trouver les chemins les plus courts en utilisant les codes Open Source.
+
+    @author BIN WAN ENDOK & BOP
+    @version 1.1 05/12/2018 
+*/
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!";
+}
